c2c0a0425a22dfa47bd3e447fdd202cfee83873b
L <danteandroi@gmail.com>
2023-09-01 12:41:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fakedata/data/MyDataClass.kt
@@ -0,0 +1,24 @@
+package com.example.fakedata.data
+
+import com.example.fakedata.FakeData
+import java.time.LocalDate
+import java.time.LocalDateTime
+
+/**
+ * @author Du Wenyu
+ * 2023/9/1
+ */
+@FakeData
+data class MyDataClass(
+    val name: String,
+    val age: Int,
+    val height: Long,
+    val job: String,
+    val time: LocalDate
+){
+    companion object{
+        fun test(){
+//            MyDataClass
+        }
+    }
+}
