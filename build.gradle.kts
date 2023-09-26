bfdf24549fb4a88cc4e74cbda4cb679cd5c2c715
L <danteandroi@gmail.com>
2023-09-26 18:18:58 +0800

    gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,14 @@
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.0.1" apply false
+    id("com.android.library") version "8.0.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
     kotlin("jvm") version "1.9.0" apply false
-    id("com.android.library") version "8.0.1" apply false
 }
 
-buildscript {
-    dependencies {
-        classpath(kotlin("gradle-plugin", version = "1.9.0"))
-    }
-}
+//buildscript {
+//    dependencies {
+//        classpath(kotlin("gradle-plugin", version = "1.9.0"))
+//    }
+//}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,12 @@
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
         maven { setUrl("https://jitpack.io") }
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
